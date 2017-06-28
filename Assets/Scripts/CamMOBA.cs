28086bb0e6f10b0658e17fbeb4a5e18d6097d3c0
Kyle Xiong <kxiong@cs.cmu.edu>
2017-06-28 01:44:57 -0400

    Can do networking with basic ground-level movement, navigation, and animation.

---
+++ Assets/Scripts/CamMOBA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMOBA : MonoBehaviour {
+
+	[SerializeField] Transform target;
+	public Vector3 cameraOffset = new Vector3(4f, 6f, 4f);
+	public float cameraSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+		if (target) {
+			transform.position = target.position + cameraOffset;
+		}
+	}
+
+	public void SetTarget(Transform t) {
+		this.target = t;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+[RequireComponent (typeof(NavMeshAgent))]
+[RequireComponent (typeof(Animator))]
+public class PlayerController : NetworkBehaviour {
+
+	[SyncVar]
+	Vector3 realPosition = Vector3.zero;
+	[SyncVar]
+	Quaternion realRotation;
+	[SyncVar]
+	float syncFwd;
+
+	Animator anim;
+	CharacterController cc;
+	NavMeshAgent nma;
+
+	int groundLayer;
+	float updateInterval;
+
+	public override void OnStartLocalPlayer() {
+		CamMOBA cm = FindObjectOfType<CamMOBA> () as CamMOBA;
+		cm.SetTarget (transform);
+	}
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+		cc = GetComponent<CharacterController> ();
+		nma = GetComponent<NavMeshAgent> ();
+
+		groundLayer = 1 << LayerMask.NameToLayer ("Ground");
+	}
+
+	void Update() {
+		MouseInput ();
+		Animate ();
+	}
+
+	void MouseInput() {
+		if (isLocalPlayer) {
+			if (Input.GetMouseButton (0)) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+				if (Physics.Raycast (ray, out hit, Mathf.Infinity, groundLayer)) {
+					nma.SetDestination (hit.point);
+				}
+			}
+
+			updateInterval += Time.deltaTime;
+			if (updateInterval > 0.11f) {
+				updateInterval = 0f;
+				CmdSync (transform.position, transform.rotation);
+			}
+		} else {
+			transform.position = Vector3.Lerp (transform.position, realPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp (transform.rotation, realRotation, 0.1f);
+		}
+	}
+
+	void Animate() {
+		if (isLocalPlayer) {
+			float forward = nma.velocity.magnitude / nma.speed;
+			anim.SetFloat ("forward", forward);
+
+			CmdSyncAnim (forward);
+		} else {
+			float forward = anim.GetFloat ("forward");
+			forward = Mathf.Lerp (forward, syncFwd, 0.1f);
+			anim.SetFloat ("forward", forward);
+		}
+	}
+
+	[Command]
+	void CmdSync(Vector3 position, Quaternion rotation) {
+		realPosition = position;
+		realRotation = rotation;
+	}
+
+	[Command]
+	void CmdSyncAnim(float fwd) {
+		syncFwd = fwd;
+	}
+}
